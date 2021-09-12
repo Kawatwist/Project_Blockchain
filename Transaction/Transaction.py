91c03eb0499fb9439e73c87bb0850e4d488f020f
Loic MASSE <lomasse@e1r1p1.42.fr>
2021-09-12 16:44:17 +0200

    Starting

---
+++ Transaction/Transaction.py
@@ -0,0 +1,34 @@
+import random
+random.seed(0)
+
+    # Random Transaction
+def makeTransaction(maxValue=3):
+    sign      = int(random.getrandbits(1))*2 - 1
+    amount    = random.randint(1,maxValue)
+    pay       = sign * amount
+    receive   = -1 * pay
+    return {u'Alice':pay,u'Bob':receive}
+
+    # Update with new entry
+def updateState(txn, state):
+    state = state.copy() # As dictionaries are mutable, let's avoid any confusion by creating a working copy of the data.
+    for key in txn:
+        if key in state.keys():
+            state[key] += txn[key]
+        else:
+            state[key] = txn[key]
+    return state
+
+    # Check the Validity
+def isValidTxn(txn,state):
+    if sum(txn.values()) is not 0:
+        return False
+    for key in txn.keys():
+        if key in state.keys(): 
+            acctBalance = state[key]
+        else:
+            acctBalance = 0
+        if (acctBalance + txn[key]) < 0:
+            return False
+    
+    return True
---
+++ main.py
@@ -0,0 +1,43 @@
+import Chain.Chain as Chain
+import Transaction.Hash as Hash
+import Transaction.Transaction as Trade
+import json
+
+def main():
+    txnBuffer = [Trade.makeTransaction() for i in range(30)]
+    state = {u'Alice':50, u'Bob':50}  # Define the initial state (Give money)
+    genesisBlockTxns = [state]
+    genesisBlockContents = {u'blockNumber':0,u'parentHash':None,u'txnCount':1,u'txns':genesisBlockTxns}
+    genesisHash = Hash.hashMe( genesisBlockContents )
+    genesisBlock = {u'hash':genesisHash,u'contents':genesisBlockContents}
+    genesisBlockStr = json.dumps(genesisBlock, sort_keys=True)
+    chain = [genesisBlock]
+    
+    blockSizeLimit = 5  # Arbitrary number of transactions per block- 
+                #  this is chosen by the block miner, and can vary between blocks!
+
+    while len(txnBuffer) > 0:
+        bufferStartSize = len(txnBuffer)
+        
+        ## Gather a set of valid transactions for inclusion
+        txnList = []
+        while (len(txnBuffer) > 0) & (len(txnList) < blockSizeLimit):
+            newTxn = txnBuffer.pop()
+            validTxn = Trade.isValidTxn(newTxn,state) # This will return False if txn is invalid
+            
+            if validTxn:           # If we got a valid state, not 'False'
+                txnList.append(newTxn)
+                state = Trade.updateState(newTxn,state)
+            else:
+                print("Transaction Refused")
+                sys.stdout.flush()
+                continue  # This was an invalid transaction; ignore it and move on
+            
+        ## Make a block
+        myBlock = Chain.makeBlock(txnList,chain)
+        chain.append(myBlock)
+        for CChain in chain:
+            print(CChain)
+
+if __name__ == '__main__':
+    main()
