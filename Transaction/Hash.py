59ccccdf3facf3586c2d7a65b87d28df0055484d
Loic MASSE <lomasse@e2r2p2.42.fr>
2021-09-19 15:02:40 +0200

    reset project

--- Transaction/Hash.py
+++
@@ -1,6 +0,0 @@
-import hashlib, json
-
-def hashMe(msg=""):
-    if type(msg)!=str:
-        msg = json.dumps(msg, sort_keys=True)  # If we don't sort keys, we can't guarantee repeatability!
-    return hashlib.sha256(str(msg).encode('utf-8')).hexdigest()
--- main.py
+++ main.py
@@ -1,107 +1,18 @@
-import Chain.Chain as Chain
-import Transaction.Hash as Hash
-import Transaction.Transaction as Trade
-import json, sys, re, socket, time
-
-from threading import Thread
-from time import sleep
-import ast
-clients = []
-txnBuffer = []
-timeout = 50
-
-def threadConnection(arg):
-    print("Waiting Clients ...")
-    while True :
-        c, addr = arg.accept()
-        print ('New Clients : ', addr)
-        clients.append({"c":c, "addr":addr, "timeout":time.time()})
-        sleep(0.5)
-
-def threadClient(arg):
-    while True :
-        for client in clients :
-            newmsg = client.get("c").recv(1024).decode()
-            if len(newmsg) != 0 :
-                print ("===========> client ", client.get("addr"), " : ", newmsg, "\n")
-                msg = ast.literal_eval(newmsg)
-                txnBuffer.append(Trade.makeRealTransaction(msg.get("price"), msg.get("pay"), msg.get("payed"), msg.get("pricepayed")))
-                client["timeout"] = time.time()
-
-            if time.time() - client.get("timeout")  > timeout :
-                print("Remove client ", client.get("addr"), " Timeout ", "%.2f" % round(time.time() - client.get("timeout"), 2), " > ", timeout)
-                clients.remove(client)
-        sleep(0.5)
-
-def threadTermFunct(arg):
-    while True :
-        for line in sys.stdin :
-            if line == 'Bal\n' :
-                print("Balance : ", balance)
-            if line == 'Cl\n' :
-                print("Clients : ", clients)
-            print("Line : ", line)
-        sleep(0.5)
-
+from ConfigClass import Config as cf
+import argparse
 
 def main():
-    # global state = {u'Pool':5000000}
-    state = {u'Pool':50000000}  # Define the initial state (Give money)
-    global balance
-    balance = state
-    genesisBlockTxns = [state]
-    genesisBlockContents = {u'blockNumber':0,u'parentHash':None,u'txnCount':1,u'txns':genesisBlockTxns}
-    genesisHash = Hash.hashMe( genesisBlockContents )
-    genesisBlock = {u'hash':genesisHash,u'contents':genesisBlockContents}
-    genesisBlockStr = json.dumps(genesisBlock, sort_keys=True)
-    chain = [genesisBlock]
-    
-    blockSizeLimit = 5  # Arbitrary number of transactions per block- 
-                #  this is chosen by the block miner, and can vary between blocks!
+    parser = argparse.ArgumentParser(description='Basic Blockchain')
+    parser.add_argument('--node', required=True, type=str, help='Choose the node type (Set node to help to display the possibility)')
+    parser.add_argument('--port', required=False, type=int, help='Blockchain Port (The fullnode port connection, initialised at 4242)')
+    parser.add_argument('--host', required=False, type=str, help='Host ip (select the url of the original node, initialised at localhost)')
+    parser.add_argument('--portCl', required=False, type=int, help='Listener port of the Node (Used for the client connection, initialised at 4243)')
+    args = parser.parse_args()
 
-    # Create Connection
-    s = socket.socket()
-    host = socket.gethostname()
-    port = 4242
-    s.bind((host, port))
-    s.listen(5)
-    print("Connection ready ", host, " At Port ", port)
-
-    threadTerm = Thread(target = threadTermFunct, args = (s,))
-    threadTerm.start()
-
-    threadCo = Thread(target = threadConnection, args = (s,))
-    threadCo.start()
-
-    threadCl = Thread(target = threadClient, args = (s,))
-    threadCl.start()
-
-
-    while True :
-        while len(txnBuffer) > 0:
-            bufferStartSize = len(txnBuffer)
-            
-            ## Gather a set of valid transactions for inclusion
-            txnList = []
-            while (len(txnBuffer) > 0) & (len(txnList) < blockSizeLimit):
-                newTxn = txnBuffer.pop()
-                validTxn = Trade.isValidTxn(newTxn,state) # This will return False if txn is invalid
-                print(validTxn)
-                if validTxn:           # If we got a valid state, not 'False'
-                    print("Transaction Validate ", newTxn)
-                    txnList.append(newTxn)
-                    state = Trade.updateState(newTxn,state)
-                else:
-                    print("Transaction Refused")
-                    sys.stdout.flush()
-                    continue  # This was an invalid transaction; ignore it and move on
-
-            ## Make a block
-            myBlock = Chain.makeBlock(txnList,chain)
-            chain.append(myBlock)
-            print("\nState : ", state)
-        balance = state
-        sleep(2)
+    config = cf()
+    config.setArgs(args)
+    config.createNode()
+    config.node.run()
 
 if __name__ == '__main__':
     main()
